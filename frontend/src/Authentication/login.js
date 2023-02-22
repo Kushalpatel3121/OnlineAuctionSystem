99bbabaf87af703cec98a388ba7b8cfc52453d57
Kushalpatel3121 <kushal.patel3121@gmail.com>
2023-02-22 15:02:42 +0530

    Frontend -v1.1
    Authentication pages UI updated

--- frontend/src/Authentication/login.js
+++ frontend/src/Authentication/login.js
@@ -1,46 +1,44 @@
+// import React from 'react';
 import { Outlet, Link } from "react-router-dom";
 
-function Login() {
-    return (
-        <>
-            <div className="flex flex-row min-h-screen font-source-sans-pro bg-[#fb8500]">
-                <div className="bg-[#ffdd87] basis-4/7 text-black rounded-tr-3xl rounded-br-3xl flex items-center justify-center select-none">
-                    <img src="/images/Auction-bro.png" alt="Auction" className="w-3/4 transition ease-in-out delay-150 hover:w-4/5 drop-shadow-xl shadow-black"></img>
-                </div>
-                <div className="bg-[#121930] basis-3/7 text-black flex flex-col items-center justify-center content-around gap-12 ml-3 rounded-tl-3xl rounded-bl-3xl">
-                    <div className="flex flex-col content-around backdrop-blur-md bg-white/30 p-6 rounded-2xl shadow-2xl shadow-white/30">
-                        <div className="self-center select-none mb-7">
-                            <h1 className="text-xl font-bold underline underline-offset-4 text-white">Welcome Back!</h1>
+const Login = () => {
+  return (
+    <>
+        <div className='flex flex-row min-h-screen'>
+            <div className='basis-8/12 bg-login-bg-2 bg-cover'>
+                {/* <div className='min-h-screen bg-primary-white-1 opacity-60 backdrop-blur-md drop-shadow-sm'></div> */}
+                <div className='min-h-screen bg-gradient-to-br from-secondary-sup-light-blue-2/80 to-primary-blue-light/80 opacity-80'></div>
+            </div>
+            <div className='basis-4/12 bg-secondary-sup-gray-4 flex flex-row justify-center items-center'>
+                <div className=''>
+                    <form className=''>
+                        <div className="select-none mb-7">
+                            <label for="username" className='text-md font-bold'> Username : </label>
+                            <br/>
+                            <input type="text" name='username' className='focus:outline-none border-b-2 border-b-primary-blue p-2 w-64 mt-0 rounded-sm bg-secondary-sup-gray-4 focus:border-b-2 text-sm' required></input>
                         </div>
-                        <div className="">
-                            <form method="POST" className="flex-col justify-items-content text-white">
-                                <div className="select-none">
-                                    <label for="username"/> Username :
-                                    <br/>
-                                    <input type="text" name="username" id="username" className="rounded-md mb-6 mt-1 w-64 h-7 border-solid border-2 focus:border-[#219ebc] focus:outline-none text-black p-2 text-sm"></input>
-                                </div>
-                                
-                                <div className="select-none">
-                                    <label for="password" /> Password :
-                                    <br/>
-                                    <input type="password" name="password" id="password" className="rounded-md mb-6 mt-1 w-64 h-7 border-solid border-2 focus:border-[#219ebc] focus:outline-none text-black p-2 text-sm"></input>
-                                </div>
-                                <div className="select-none">
-                                    <input type="submit" className="rounded-md bg-[#67e8f9]/75 text-black justify-self-center border-solid border-2 border-white/70 hover:border-[#121930] hover:bg-[#67e8f9]/60 mt-4 w-64 h-7" value={"Login"}></input>
-                                </div>
-                            </form>
+
+                        <div className="select-none mb-5">
+                            <label for="password" className="text-md font-bold"> Password : </label>
+                            <br/>
+                            <input type="password" name='password'className='focus:outline-none border-b-2 border-b-primary-blue p-2 w-64 mt-0 rounded-sm bg-secondary-sup-gray-4 focus:border-b-2 text-sm' required></input>
                         </div>
-                        <div className="self-center mt-2 text-gray-400 select-none">
+
+                        <div className="select-none">
+                            {/* <button type='submit'> Login </button> */}
+                            <input type="submit" value={"Login"} className="rounded-sm text-md text-primary-white-1 justify-self-center border-solid border-2 border-primary-white-1 mt-4 w-64 h-7 bg-primary-blue/75 hover:bg-primary-blue-light"></input>
+                        </div>
+
+                        <div className="text-center mt-4 text-primary-gray-1 text-sm select-none">
                             <p>Don't have an account?</p>
-                            <p className="text-center underline underline-offset-2 text-white/70"><a href="#"><Link to="/signup">Sign Up</Link></a></p>
+                            <p className="underline underline-offset-2 text-white/70"><a href="#"><Link to="/signup">Sign Up</Link></a></p>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
-
-            <Outlet />
-        </>
-    );
+        </div>
+    </>
+  )
 }
 
 export default Login;
